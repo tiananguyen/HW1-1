26ea2f397bf38270d857ad8f2b3c70ead3f96b56
tiananguyen <31391421+tiananguyen@users.noreply.github.com>
2017-09-26 22:32:56 -0700

    HW

---
+++ Classwork/HW1-2.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+/*Assume that isIsosceles is a bool variable, and that the variables isoCount, triangleCount, and
+polygonCount have all been declared and initialized. Write a statement that adds 1 to each of
+these count variables (isoCount, triangleCount, and polygonCount) if isIsosceles is true.*/
+
+main() {
+  if(isIsosceles==1) {
+	   isoCount++;
+	   triangleCount++;
+	    polygonCount++;
+  }
+}
---
+++ Classwork/HW1-3.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+/*Assume that the variables gpa, deansList and studentName, have been declared and initialized.
+Write a statement that both adds 1 to deansList and prints studentName to standard out if gpa exceeds 3.5.*/
+
+main() {
+  if(gpa > 3.5) {
+    deansList++;
+    cout<<studentName;
+  }
+}
