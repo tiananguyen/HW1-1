b2e834e178460e466bc2f3e15f3dc4dd26d5969f
tiananguyen <31391421+tiananguyen@users.noreply.github.com>
2017-10-03 12:59:40 -0700

    HW2-3

---
+++ Schoolwork/HW2-3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+/*use a while loop to print a single line consisting of 97 asterisks. Use no variables other than k.*/
+
+int main(){
+  int k;
+  k=0;
+  while(k<97){
+	 cout<<"*";
+	 k++;
+  }
+}
