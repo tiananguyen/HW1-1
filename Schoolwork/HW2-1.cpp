63b76f69e5959bd878918f4f53cc2c7c2d1657c5
tiananguyen <31391421+tiananguyen@users.noreply.github.com>
2017-10-03 15:34:07 -0700

    HW

---
+++ Schoolwork/HW2-1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+/*write a for loop that prints the integers 0 through 39, separated by spaces. Use no variables other than i.*/
+
+int main() {
+  int i;
+  for(i=0;i<40;i++) {
+	 cout<<i;
+	  cout<<" ";
+  }
+}
---
+++ Schoolwork/HW2-4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+//use a do...while loop to print a single line consisting of 97 asterisks. Use no variables other than k.
+
+int main() {
+  int k;
+  k=0;
+  do {
+	 cout<<"*";
+	  k++;
+  }while(k<97);
+}
