869a49f105c27b58d6b78510e31d747246795602
tiananguyen <31391421+tiananguyen@users.noreply.github.com>
2017-10-03 13:24:20 -0700

    HW2-2

---
+++ Schoolwork/HW2-2.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+/*use a for loop to print a single line consisting of 97 asterisks. Use no variables other than k.*/
+main(){
+  int k;
+  for(k=0;k<97;k++){
+	   cout<<"*";
+  }
+}
