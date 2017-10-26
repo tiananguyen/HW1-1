7c88ba5344c5c296d00d0d099c75ebfded08d2a8
tiananguyen <31391421+tiananguyen@users.noreply.github.com>
2017-10-25 22:41:54 -0700

    HW5.1

---
+++ Schoolwork/HW5.1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  double temps[], total, avgTemp;
+  int n;
+  total=0;
+  for (k=0; k<n; k++) {
+    total += temps[k];
+  }
+  avgTemp =total/n;
+  return 0;
+}
